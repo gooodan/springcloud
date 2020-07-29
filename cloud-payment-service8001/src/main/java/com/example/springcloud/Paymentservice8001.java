01f735e3f106e577a00cb5f9e51bd7938d5ea099
gooodan <304028999@qq.com>
2020-07-29 18:52:38 +0800

    add

---
+++ cloud-payment-service8001/src/main/java/com/example/springcloud/Paymentservice8001.java
@@ -0,0 +1,11 @@
+package com.example.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Paymentservice8001 {
+    public static void main(String[] args) {
+        SpringApplication.run(Paymentservice8001.class, args);
+    }
+}
---
+++ cloud-payment-service8001/src/main/java/com/example/springcloud/dao/PaymentDao.java
@@ -0,0 +1,5 @@
+package com.example.springcloud.dao;
+
+public interface PaymentDao {
+
+}
